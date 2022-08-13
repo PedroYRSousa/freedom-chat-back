9a2306d63b0a6d60468ecfa407045ea708fc082b
yagosousa2512 <yagosousa2512@gmail.com>
2022-08-13 23:57:26 +0000

    MySocket completed

---
+++ src/Socket/MySocket/MySocket.ts
@@ -0,0 +1,31 @@
+import Log from "../../Log/Log";
+import { Socket } from "socket.io";
+
+export default class MySocket {
+    private id = "";
+    private socket: Socket;
+
+    constructor(socket: Socket) {
+        this.id = socket.id;
+        this.socket = socket;
+    }
+
+    public get Id() { return (this.id) };
+    public get Socket() { return (this.socket) };
+
+    protected on(label: string) {
+        this.log(`on ${label}`);
+
+        this.Socket.on(label, (body) => (this as any)[label](body));
+    }
+
+    protected emit(label: string, args: Array<any>) {
+        this.log(`emit ${label}`);
+
+        this.Socket.emit(label, args);
+    }
+
+    protected log(message: string) {
+        Log.log(`${this.Id} ${message}`)
+    }
+}
