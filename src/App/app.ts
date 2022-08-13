4124b0e6e178e4a61a9d0aa927f4015f8d1ace48
yagosousa2512 <yagosousa2512@gmail.com>
2022-08-13 23:15:25 +0000

    app user config

--- src/App/app.ts
+++ src/App/app.ts
@@ -4,6 +4,10 @@ import { Express, Request, Response } from 'express';
 export default (): Express => {
     const app = express();
 
+    app.use(express.json());
+    app.use(express.static('public'));
+    app.use(express.urlencoded({ extended: false }));
+
     app.get('', (req: Request, res: Response) => {
         res.sendFile(__dirname + '/index.html');
     })
