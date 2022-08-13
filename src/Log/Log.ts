1f3242f9e543ef97950bf5c0c970672c29750662
yagosousa2512 <yagosousa2512@gmail.com>
2022-08-13 23:50:12 +0000

    Log fixed

--- src/Log/Log.ts
+++ src/Log/Log.ts
@@ -1,15 +1,22 @@
 export default class Log {
-    static log(message: string) {
+    public static log(message: string) {
         const date = new Date();
-        const day = date.getDate();
-        const month = date.getMonth();
-        const fullYear = date.getFullYear();
-        const hours = date.getHours();
-        const minutes = date.getMinutes();
-        const seconds = date.getSeconds();
+        const day = Log.Adjuste(date.getDate().toString());
+        const month = Log.Adjuste(date.getMonth().toString());
+        const fullYear = Log.Adjuste(date.getFullYear().toString());
+        const hours = Log.Adjuste(date.getHours().toString());
+        const minutes = Log.Adjuste(date.getMinutes().toString());
+        const seconds = Log.Adjuste(date.getSeconds().toString());
 
         const dateString = `${month}/${day}/${fullYear} - ${hours}:${minutes}:${seconds}`;
 
         console.log(`[${dateString}] -- ${message}`)
+    }
+
+    private static Adjuste(target: string) {
+        if (target.length < 2)
+            target = '0' + target;
+
+        return (target);
     }
 }
