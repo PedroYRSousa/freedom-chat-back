a8ee5f74e044b94142a183e64613b14872f7c913
yagosousa2512 <yagosousa2512@gmail.com>
2022-08-14 00:05:57 +0000

    cors config

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -3,7 +3,11 @@ import { Server } from "socket.io";
 import Connection from './Connection/Connection';
 
 export default (server: http.Server): Server => {
-    const io = new Server(server);
+    const io = new Server(server, {
+        cors: {
+            origin: "*"
+        }
+    });
 
     io.on('connection', Connection.connection);
 
