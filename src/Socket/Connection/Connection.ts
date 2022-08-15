59e25f4c82af5da2e1fe4e0eb8aa1f2aebfef0e3
yagosousa2512 <yagosousa2512@gmail.com>
2022-08-15 03:11:37 +0000

    Server don't save the chats

--- src/Socket/Connection/Connection.ts
+++ src/Socket/Connection/Connection.ts
@@ -1,19 +1,15 @@
 import { Socket } from 'socket.io'
-import Chat from '@Socket/Chat/Chat'
 import MySocket from '@Socket/MySocket/MySocket'
 
 export default class Connection extends MySocket {
   private static countConnectionsInstance = 0
   private static connectionsInstance: { [id: string]: Connection } = {}
 
-  private contactSelected: string = ''
-
   constructor (socket: Socket) {
     super(socket)
 
     this.log('connected')
 
-    this.on('askChat')
     this.on('addMessage')
     this.on('disconnect')
     this.on('getContacts')
@@ -26,31 +22,12 @@ export default class Connection extends MySocket {
     new Connection(socket)
   }
 
-  public get ContactSelected (): string { return this.contactSelected }
-
-  private askChat (body: any): void {
-    const { contactId } = body
-
-    if (Connection.connectionsInstance[contactId] === undefined) { return }
-
-    const chat = Chat.getChat(this.Id, contactId)
-
-    this.contactSelected = contactId
-    this.emit('getChat', { chat })
-  }
-
   private addMessage (body: any): void {
     const { contactId, message } = body
 
     if (Connection.connectionsInstance[contactId] === undefined) { return }
 
-    Chat.addMessage(this.Id, contactId, message)
-
-    const chat = Chat.getChat(this.Id, contactId)
-
-    if (Connection.connectionsInstance[contactId].ContactSelected === this.Id) { Connection.connectionsInstance[contactId].emit('getChat', { chat }) } else { Connection.connectionsInstance[contactId].emit('alertNewContentChat', { contactId: this.Id }) }
-
-    this.emit('getChat', { chat })
+    Connection.connectionsInstance[contactId].emit('newMessage', { author: this.Id, message })
   }
 
   private disconnect (): void {
@@ -75,7 +52,6 @@ export default class Connection extends MySocket {
     // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
     delete Connection.connectionsInstance[instance.Id]
     Connection.countConnectionsInstance--
-    Chat.removeChat(instance.Id)
     instance.broadcast('removeContact', { contact: instance.Id })
   }
 
